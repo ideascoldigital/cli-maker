8d97e28f9620db923bc3ed6659c0c13fe503d358
Jairo Fernandez <jairofg12@gmail.com>
2024-07-19 00:53:56 -0500

    chore: updating data

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -61,20 +61,22 @@ const templateBin = `#!/usr/bin/env node
 require('../cli.js');
 `;
 
-const templateCommand = `import { Command } from '@ideascol/cli-maker';
+const templateCommand = `import { Command, ParamType } from '@ideascol/cli-maker';
 import { Greet } from '../lib';
 
 let commandGreet: Command = {
   name: 'greet',
   description: 'Greet the user',
-  params: [{ name: 'name', description: 'The name of the user to greet' }],
+  params: [
+    {
+      name: 'name',
+      description: 'The name of the user to greet',
+      required: true,
+      type: ParamType.Text
+    }],
   action: (args) => {
     const name = args.name;
-    if (name) {
-      Greet(name);
-    } else {
-      console.log('Error: Name parameter is required');
-    }
+    Greet(name);
   }
 }
 
